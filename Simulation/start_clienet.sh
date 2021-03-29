5ee335cda2f5594103e4eed0d389985cd063001b
kepet19 <kevinmeejach@gmail.com>
2021-03-29 21:39:33 +0200

    change the port from 8100 to 81

--- Simulation/start_clienet.sh
+++ Simulation/start_clienet.sh
@@ -2,4 +2,4 @@
 wine Config1/X20CP1586/ar000loader.exe &
 sleep 5
 notify-send "Remember to refresh the page" "If it is not loadet yet"
-xdg-open http://localhost:8100/index.html?visuId=visu
+xdg-open http://localhost:81/index.html?visuId=visu
